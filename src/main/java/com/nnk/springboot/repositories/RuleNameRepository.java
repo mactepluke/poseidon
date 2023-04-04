b261a93f76469657315aa9c51cdbc0c1357ea4f8
Jean-Luc METZ <116441043+mactepluke@users.noreply.github.com>
2023-04-04 13:44:13 +0200

    Created entities fields w/ getters/setters, added entity mapping to database tables, upgraded Maven dependencies (to Spring 3, javax -> jakarta, etc.)

--- src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
+++ src/main/java/com/nnk/springboot/repositories/RuleNameRepository.java
@@ -1,8 +1,10 @@
-package com.nnk.springboot.repositories;
+package com.nnk.springboot.repository;
 
 import com.nnk.springboot.domain.RuleName;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface RuleNameRepository extends JpaRepository<RuleName, Integer> {
 }
